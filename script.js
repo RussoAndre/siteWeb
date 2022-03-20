e62674bfa32083d1163e2d4b732334f7f59f9628
André Russo Windows <andreprusso7@gmail.com>
2022-03-19 22:32:11 -0300

    Site calculando IMC

--- script.js
+++ script.js
@@ -1,46 +1,33 @@
-
-let dateControl = document.querySelector('input[type="date"]');
-if(dateControl){
-  let date = new Date();
-
-  let day = String(date.getDate()).padStart(2, '0');
-  let month = String(date.getMonth() + 1).padStart(2, '0');
-  let year = date.getFullYear();
-
-  currentData = year + '-' + month + '-' + day;
-
-  dateControl.value = currentData;
-}
-
 function CalcIMC() {
-  var inputWeight = document.getElementById("weight").value;
-  var inputHeight = document.getElementById("height").value;
+  var peso = document.getElementById("peso").value;
+  var altura = document.getElementById("altura").value;
 
-  if (inputHeight.includes(",")) {
-    alert("Não usa , não loko se vai quebrar o sistema para de graça e coloca .")
+  if (altura.includes(",")) {
+    alert("troque ',' por '.'")
     
     return
   }
 
-  var IMC = inputWeight / (inputHeight * inputHeight)
+  var IMC = peso / (altura * altura);
 
   console.log(IMC)
 
   if (IMC < 18.5) {
     alert("Seu IMC é de " + IMC.toFixed(2) + " CLASSIFICAÇÃO MAGREZA");
-  } else if (IMC > 18.5 && IMC <= 24.9) {
+  } else if (IMC > 18.5 && IMC < 25) {
     alert("Seu IMC é de " + IMC.toFixed(2) + " CLASSIFICAÇÃO NORMAL");
-  } else if (IMC > 25 && IMC <= 29.9) {
+  } else if (IMC >= 25 && IMC < 30) {
     alert("Seu IMC é de " + IMC.toFixed(2) + " CLASSIFICAÇÃO SOBREPESO");
-  } else if (IMC > 30 && IMC <= 39.9) {
+  } else if (IMC >= 30 && IMC < 40) {
     alert("Seu IMC é de " + IMC.toFixed(2) + " CLASSIFICAÇÃO OBESIDADE");
-  } else if (IMC > 40 && IMC <= 80) {
+  } else if (IMC >= 40 && IMC < 80) {
     alert("Seu IMC é de " + IMC.toFixed(2) + " CLASSIFICAÇÃO OBESIDADE GRAVE");
-  } else if (IMC > 900000) {
-    alert("Coloca as informações certas ai véi");
+  } else if (IMC > 90000) {
+    alert("Informações incorretas");
   } else {
     alert("Digite os dados corretamente!");
   }
 
   return IMC;
 }
+
